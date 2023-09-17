11206565df858020a29cf451f1878fe8cb206229
catdisk04 <aldisdaniel04@gmail.com>
2023-09-17 19:59:15 +0530

    Added CDS field

--- myapp/engine.py
+++ myapp/engine.py
@@ -4,10 +4,10 @@ org_choices = []
 for i in organism_choices:
     org_choices.append((i, i))
 
-def optimise(rbs_seq, target, temp, organism):
+def optimise(rbs_seq, target, temp, organism, cds_seq):
     return {"result_seq": "AAAA", "achieved_rate": 10}
 
-def predict(rbs_seq,temp, organism):
+def predict(rbs_seq,temp, organism, cds_seq):
     return {"result_rate": 10}
 
 def is_valid_seq(rbs_seq):
--- myapp/forms.py
+++ myapp/forms.py
@@ -3,11 +3,13 @@ from .engine import org_choices
 
 class Optimise_input(forms.Form):
     rbs_seq = forms.CharField(label="RBS Sequence")
+    cds_seq = forms.CharField(label="Coding Sequence")
     target_rate = forms.DecimalField(label = "Target rate")
     temp = forms.DecimalField(label="Temperature (C)")
     organism = forms.ChoiceField(label="Organism", choices = org_choices)
 
 class Predict_input(forms.Form):
     rbs_seq = forms.CharField(label="RBS Sequence")
+    cds_seq = forms.CharField(label="Coding Sequence")
     temp = forms.DecimalField(label="Temperature (C)")
     organism = forms.ChoiceField(label="Organism", choices = org_choices)
